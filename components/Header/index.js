e26b9d6ec33eafe3a7679d35a62612f8f083caa4
Jay Leach <42565053+leachcoding@users.noreply.github.com>
2019-08-16 21:02:24 -0400

    Final editing -- answering questions

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,35 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {}
+const headerArray = ["SMARCH 28, 2019", "Lambda Times", "98°"];
+
+const headerContainer = document.querySelector('.header-container');
+
+function Header(child1, child2, child3) {
+  // This will define new elements:
+  const header = document.createElement('div');
+  const span1 = document.createElement('span');
+  const h1Header = document.createElement('h1');
+  const span2 = document.createElement('span');
+
+  // Structuring the elements:
+  // Append header to the headerContainer
+  headerContainer.appendChild(header);
+  header.appendChild(span1);
+  header.appendChild(h1Header);
+  header.appendChild(span2);
+
+  // Set class names
+  header.classList.add('header');
+  span1.classList.add('date');
+  span2.classList.add('temp');
+
+  // Set text content
+  span1.textContent = child1;
+  h1Header.textContent = child2;
+  span2.textContent = child3;
+
+  return header;
+}
+
+Header(headerArray[0], headerArray[1], headerArray[2]);
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -2,8 +2,42 @@
 // -----------------------
 // Using axios send a GET request to the address: https://lambda-times-backend.herokuapp.com/topics
 // Once the data is returned console.log it and review the structure.
+// ["javascript", "bootstrap", "technology", "jquery", "node.js"]
 // Iterate over the topics creating a new Tab component and add it to the DOM
 // under the .topics element.
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+let topics = [];
+
+axios
+  .get('https://lambda-times-backend.herokuapp.com/topics')
+  .then(response => {
+    console.log(response);
+    // Handles Success
+    response.data.topics.forEach(element => {
+      topics.push(element);
+    })
+    topics.forEach(element => {
+      tabs.appendChild(tabCreate(element));
+    })
+  })
+  .catch(error => {
+    console.log(error);
+  })
+
+  console.log(topics);
+
+
+
+
+const tabs = document.querySelector('.topics');
+
+function tabCreate(element) {
+  const tab = document.createElement('div');
+  tab.classList.add('tab');
+  tab.textContent = element;
+
+  return tab;
+}
